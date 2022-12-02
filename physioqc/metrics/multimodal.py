f8f7e4d07fa119a004a4537fd5f6415a8ba041e8
neuralkn0t <neuralkn0t@protonmail.com>
2022-12-02 11:04:40 +0100

    Add std to multimodal.py

--- physioqc/metrics/multimodal.py
+++ physioqc/metrics/multimodal.py
@@ -1,8 +1,28 @@
 """These functions compute RETROICOR regressors (Glover et al. 2000)."""
+# import file
 import numpy as np
 
 from .. import references
 from ..due import due
 
 
-pass
+def std(signal):  
+    """
+    Calculate standard deviation across input channels of signal.
+    
+    Parameters
+    ----------
+    std : function
+        Calculate standard deviation across input channels
+    args : signal
+        ND array with signal of some human biometric data, hopefully from a living human.
+        
+    Returns
+    -------
+    std_val : N-sized array
+        Standard deviation of signal.
+
+    """
+
+     std_val = np.std(signal, axis = 0)
+    return std_val
