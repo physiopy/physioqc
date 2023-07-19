246ff67f39a35370447a10afd993049c4d25eaf4
SRSteinkamp <simon.steinkamp@googlemail.com>
2023-07-19 22:55:01 +0200

    preprocessing and peak detection for further metrics

--- physioqc/metrics/multimodal.py
+++ physioqc/metrics/multimodal.py
@@ -1,6 +1,7 @@
 """These functions compute RETROICOR regressors (Glover et al. 2000)."""
 # import file
 import numpy as np
+import peakdet as pk
 
 from .. import references
 from ..due import due
@@ -47,3 +48,40 @@ def tSNR(signal):
     std = np.std(signal, axis=0)
     tSNR_val = me / std
     return tSNR_val
+
+def peak_detection(data: pk.Physio, target_fs: float = 40.0, filter_cutoff: float = 2.0,
+                   filter_method: str = "lowpass", filter_order: int = 7,
+                   peak_threshold: float = 0.1, peak_dist: int = 60):
+    """
+    Perform peak detection for further metric extraction and plotting.
+
+    Parameters
+    ----------
+    data : pk.Physio
+        A peakdet Physio object
+    target_fs : float, optional
+        Sampling rate for interpolation, by default 40.0
+    filter_cutoff : float, optional
+        Filter cutoff for filtering step, by default 2.0
+    filter_method : str, optional
+        Filter for filtering step, by default "lowpass"
+    filter_order : int, optional
+        Filter order for filtering step, by default 7
+    peak_threshold : float, optional
+        Threshold for peak detection, by default 0.1
+    peak_dist : int, optional
+        Distance for peak detection, by default 60
+
+    Returns
+    -------
+    pk.Physio
+        Updated pk.Physio class with peaks etc.
+    """
+    # Downsample the signal - we don't need more than 40 Hz with our MRI data sampling
+    ph = pk.operations.interpolate_physio(data, target_fs, kind="linear")
+    # Apply a lowpass filter to remove the extreme high frequencies
+    ph = pk.operations.filter_physio(ph, filter_cutoff, method=filter_method, order=filter_order)
+    ph = pk.operations.peakfind_physio(ph, thresh=peak_threshold, dist=peak_dist)
+
+    return ph
+
